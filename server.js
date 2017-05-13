0022d3ea77b2625653142ce4497db081347e181b
Zakariya Moallin <puntnomads@gmail.com>
2017-05-13 14:55:06 +0000

    finished the functionality

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:time', function (req, res) {
+  var param = req.params.time;
+  var unix;
+  var natural;
+  if(Number(param)>0){
+    unix = Number(param);
+    var date = new Date(unix*1000);
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var year = date.getFullYear();
+    var month = months[date.getMonth()];
+    var day = date.getDate();
+    "December 15, 2015"
+    natural = month + " " + day + ", " + year;
+  } else {
+    unix = (new Date(param)).getTime()/1000;
+    natural = param;
+  }
+  var object = { unix: unix, natural: natural};
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(object));
 });
 
 app.listen(8080, function () {
