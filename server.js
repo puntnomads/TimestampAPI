8f56ca4c47ae573d8ddd8bff79b06119091b8abf
Zakariya Moallin <puntnomads@gmail.com>
2017-06-15 09:54:46 +0000

    added render to get route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.sendFile('index');
+  res.render('index');
 })
 
 app.get('/:time', function (req, res) {
