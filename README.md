d75645b45cd20fb17945d1a09443498122f81ffa
Zakariya Moallin <puntnomads@gmail.com>
2017-06-15 08:10:13 +0000

    added static files

---
+++ README.md
@@ -0,0 +1,1 @@
+This API allows you to send a string that is either a unix timestamp or a natural language date and it will return both a unix timestamp and a natural language date.
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var path = require('path');
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('index.html');
 })
 
 app.get('/:time', function (req, res) {
